16db9c204ac5b2c231e8b812e39137fc32b06107
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-10 09:59:18 +0300

    with ejs template and veriables and adjusted server code

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
 
     if (currentDay === 6 || currentDay === 0){
-        res.sendFile(__dirname + '/weekend.html');
+        var day = 'Weekend 😀';
+        var desc = 'taste it while you can';
+        res.render('list', {typeOfDay : day, about : desc});
     } else {
-        res.sendFile(__dirname + '/workday.html');
+        var day = 'Weekday 🥸';
+        var desc = 'wait a little bit more';
+        res.render('list', {typeOfDay : day, about : desc});
     }
 });
 
