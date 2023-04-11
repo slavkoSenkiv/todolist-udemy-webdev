1fd3d61b1d92f269cfa2405709ad8fb5db7b00c1
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-11 07:59:04 +0300

    add scriplet with h1 color change based on dow

--- app.js
+++ app.js
@@ -9,44 +9,42 @@ app.get('/', function(req, res){
 
     switch(currentDay){
         case 0 :
-            var day = 'Sunday 😀';
+            var day = 'Sunday';
             var desc = '1 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 1 :
-            var day = 'Monday 😀';
+            var day = 'Monday';
             var desc = '2 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 2 :
-            var day = 'Tuesday 😀';
+            var day = 'Tuesday';
             var desc = '3 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 3 :
-            var day = 'Wednessday 😀';
+            var day = 'Wednessday';
             var desc = '4 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 4 :
-            var day = 'the  😀';
+            var day = 'Thursday';
             var desc = '5 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 5 :
-            var day = 'fri 😀';
+            var day = 'Friday';
             var desc = '6 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         case 6 :
-            var day = 'sat 😀';
+            var day = 'Saturday';
             var desc = '7 taste it while you can';
             res.render('list', {typeOfDay : day, about : desc});
             break;
         default:
-            var day = 'idk this day 😀';
-            var desc = '8 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
+            console.log('the error with day num, it is not int between 0 and 6')
             break;
     }   
 });
