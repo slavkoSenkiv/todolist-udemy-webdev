b73b6bef37ba95d9120953e48298439f3d9204c8
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-11 08:03:02 +0300

    cut switch code

--- app.js
+++ app.js
@@ -10,43 +10,30 @@ app.get('/', function(req, res){
     switch(currentDay){
         case 0 :
             var day = 'Sunday';
-            var desc = '1 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         case 1 :
             var day = 'Monday';
-            var desc = '2 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         case 2 :
             var day = 'Tuesday';
-            var desc = '3 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         case 3 :
-            var day = 'Wednessday';
-            var desc = '4 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
+            var day = 'Wednesday';
             break;
         case 4 :
             var day = 'Thursday';
-            var desc = '5 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         case 5 :
             var day = 'Friday';
-            var desc = '6 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         case 6 :
             var day = 'Saturday';
-            var desc = '7 taste it while you can';
-            res.render('list', {typeOfDay : day, about : desc});
             break;
         default:
             console.log('the error with day num, it is not int between 0 and 6')
             break;
-    }   
+    }  
+    res.render('list', {typeOfDay : day});
 });
 
 app.listen(3000, function(){
