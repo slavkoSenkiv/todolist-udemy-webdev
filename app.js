1d61dc978b33a4a9a82e109a4d6f48b36c275319
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-11 08:59:33 +0300

    decrease code with better date method

--- app.js
+++ app.js
@@ -5,35 +5,16 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
 
-    switch(currentDay){
-        case 0 :
-            var day = 'Sunday';
-            break;
-        case 1 :
-            var day = 'Monday';
-            break;
-        case 2 :
-            var day = 'Tuesday';
-            break;
-        case 3 :
-            var day = 'Wednesday';
-            break;
-        case 4 :
-            var day = 'Thursday';
-            break;
-        case 5 :
-            var day = 'Friday';
-            break;
-        case 6 :
-            var day = 'Saturday';
-            break;
-        default:
-            console.log('the error with day num, it is not int between 0 and 6')
-            break;
-    }  
-    res.render('list', {typeOfDay : day});
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    var currentDay = today.toLocaleDateString('en-US', options);
+ 
+    res.render('list', {currentDay: currentDay});
 });
 
 app.listen(3000, function(){
