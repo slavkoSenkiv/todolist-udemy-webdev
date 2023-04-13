ef830796cbe634de62e797824a706c3bc3023354
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-13 08:58:41 +0300

    cut unnecessary lines

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 
 let personalTasksLst = ['buy food', 'cook food', 'eat food'];
 let workTasksLst = ['check calendar', 'check tasks', 'check inbox'];
-
-let  targetLst = [];
 
 const today = new Date();
 
@@ -19,36 +19,30 @@ const options = {
 const taskListDate = today.toLocaleDateString('en-US', options);
 
 
-app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-
     const taskListType = 'Personal | ';
-    
     res.render('list', {taskListType: taskListType, taskListDate: taskListDate, tasksLst: personalTasksLst});
 
-    targetLst = personalTasksLst;
 });
 
 app.get('/work', function(req, res){
-    
     const taskListType = 'Work | ';
-    
+
     res.render('list', {taskListType: taskListType, taskListDate: taskListDate, tasksLst: workTasksLst});
 
-    targetLst = workTasksLst;
 });
 
 app.post('/', function(req, res){
     const newTask = req.body.newTask;
-    targetLst.push(newTask);
+    personalTasksLst.push(newTask);
     res.redirect('/');
 });
 
 app.post('/work', function(req, res){
     const newTask = req.body.newTask;
-    targetLst.push(newTask);
+    workTasksLst.push(newTask);
     res.redirect('/work');
 });
 
