f873ead427c3f0792200f3ad1c9a312a0095f3a8
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-13 22:32:05 +0300

    make it work by angella way

--- app.js
+++ app.js
@@ -13,39 +13,46 @@ const options = {
     weekday: 'short',
     day: 'numeric',
     month: 'short'
-};
+}
 const taskListDate = today.toLocaleDateString('en-US', options);
 
 app.get('/', function(req, res){
-    const taskListType = 'Personal | ';
+    const taskListType = 'Personal';
     res.render('list', {
         taskListType: taskListType, 
         taskListDate: taskListDate, 
         tasksLst: personalTasksLst});
 });
 
-app.post('/', function(req, res){
-    console.log(req.body);
-    let newTask = req.body.newTask;
-    personalTasksLst.push(newTask);
-    res.redirect('/');
-});
-
 app.get('/work', function(req, res){
-    const taskListType = 'Work | ';
+    const taskListType = 'Work';
     res.render('list', {
         taskListType: taskListType, 
         taskListDate: taskListDate, 
         tasksLst: workTasksLst});
     });
+
+app.post('/', function(req, res){
+    let newTask = req.body.newTask;
+
+    if(req.body.plusButton === 'Work'){
+        workTasksLst.push(newTask);
+        res.redirect('/work');
+    }else{
+        personalTasksLst.push(newTask);
+        res.redirect('/')
+    }
+
+});
+
     
-app.post('/work', function(req, res){
-    console.log(req.body);
+/* app.post('/work', function(req, res){
+    console.log(req.body.plusButtonValue);
     let newTask = req.body.newTask;
     workTasksLst.push(newTask);
     res.redirect('/work');
 });
-
+ */
 app.listen(3000, function(){
     console.log('server is up and listening to port 3000');
 });
