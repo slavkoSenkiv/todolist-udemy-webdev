ed92eea9f3d0c6603fb9bcc51fa7640c771ed1bb
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-16 08:16:20 +0300

    refractor date module

---
+++ date.js
@@ -0,0 +1,23 @@
+exports.getDate = function(){
+
+    const today = new Date();
+
+    const options = {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'short'}
+
+    return today.toLocaleDateString('en-US', options);
+};
+
+exports.getDayOfWeek = function (){
+
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+}
+
+    return today.toLocaleDateString('en-US', options);
+};
+
